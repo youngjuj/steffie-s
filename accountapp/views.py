4fe69daa5234a833fea52d35ded0769ebaa96f0a
youngjuj <alti100@likelion.org>
2021-07-01 11:35:35 +0900

    2021/7/1 with teacher complete

---
+++ accountapp/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+#alt + enter 하면 오류를 알려주고 오류 해결방법을 제시해줌, 엔터하면 다 해결
+
+
+def hello_world(request):
+    return HttpResponse('Hello World!')
