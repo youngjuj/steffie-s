0b6b741426f2f4efa1433b9707d21a1a27b7cb81
youngjuj <alti100@likelion.org>
2021-07-12 12:24:00 +0900

    2021/7/12 with teacher redirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 #alt + enter 하면 오류를 알려주고 오류 해결방법을 제시해줌, 엔터하면 다 해결
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -15,10 +17,8 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all() #헬로월드라는 리스트에 헬로월드 클래스의 모든 것을 집어넣음
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
