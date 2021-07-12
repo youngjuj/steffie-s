d35e0587aec672b2337c69c472687ed247093007
youngjuj <alti100@likelion.org>
2021-07-12 11:30:06 +0900

    2021/7/12 with teacher db

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,15 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 #alt + enter 하면 오류를 알려주고 오류 해결방법을 제시해줌, 엔터하면 다 해결
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
     if request.method == "POST":
 
-        temp = request.POST.get('input_text'
-                                '')
+        temp = request.POST.get('input_text')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD'})
