565f17a06c5aabd39824eabf4e01f59cb78d6a22
youngjuj <alti100@likelion.org>
2021-07-26 11:52:00 +0900

    2021/7/26 with teacher profile forms

--- profileapp/models.py
+++ profileapp/models.py
@@ -9,6 +9,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='profile' )
 
-    image = models.ImageField(models.ImageField(upload_to='profile/', null=True)) #경로지정
+    image = models.ImageField(upload_to='profile/', null=True) #경로지정
     nickname = models.CharField(max_length=30, unique=True, null=True)
     message = models.CharField(max_length=200, null=True)
