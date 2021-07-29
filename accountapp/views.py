6a4d442887fadb4542bca813dcfe5ebb902309a7
youngjuj <alti100@likelion.org>
2021-07-29 11:49:17 +0900

    2021/7/29 with teacher reverse edit

--- accountapp/views.py
+++ accountapp/views.py
@@ -54,8 +54,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
     # def get(self, request, *args, **kwargs): #get은 method임
     #     if request.user.is_authenticated and self.get_object() == request.user:
