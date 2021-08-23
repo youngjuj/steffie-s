248301d97ac06b8e7f43d647e92f4836f054ca5b
youngjuj <alti100@likelion.org>
2021-08-23 11:44:07 +0900

    2021/8/23 subscription unsubscribe button

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -5,6 +5,6 @@ from subscribeapp.views import SubscriptionView
 app_name = 'subscribeapp'
 
 urlpatterns = [
-    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe')
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
 
 ]
