664ee37d09b6d944617032dfb2766d0578fde8a2
youngjuj <alti100@likelion.org>
2021-08-24 16:52:19 +0900

    2021/8/24 likeapp migrations

--- articleapp/models.py
+++ articleapp/models.py
@@ -17,3 +17,5 @@ class Article(models.Model):
     content = models.TextField(null=True) #긴 문자열
 
     created_at = models.DateField(auto_now_add=True, null=True)
+
+    like = models.IntegerField(default=0)
