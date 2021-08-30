52dec064a199a313c175195134e81a01e87c9319
youngjuj <alti100@likelion.org>
2021-08-30 11:17:44 +0900

    2021/8/30 final edit

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
                                related_name='article', null=True) #게시글 여러개 작성용
 
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
