c02b653a5e64ee6b5ee01e364e21e91c87c77838
youngjuj <alti100@likelion.org>
2021-09-16 11:39:41 +0900

    2021/9/16 edit

--- with_teacher/settings/base.py
+++ with_teacher/settings/base.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_DIRS = [
