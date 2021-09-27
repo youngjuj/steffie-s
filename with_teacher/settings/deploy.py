ee8e659247d5d255c1290f2d858ab6f0c62cb708
youngjuj <alti100@likelion.org>
2021-09-27 11:48:50 +0900

    2021/9/27 docker secrets deploy edit

--- with_teacher/settings/deploy.py
+++ with_teacher/settings/deploy.py
@@ -1,25 +1,16 @@
 from .base import *
 
 
-env_list = dict()
+def read_secret(secret_name):
 
-local_env = open(os.path.join(BASE_DIR, '.env'))    #운영체제 내의 경로로 베이스디렉토리랑 .env를 합쳐주는 함수를 경로로 다시 돌려줌
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')     #공백제거용
-    start = line.find('=')        #제일 먼저 나오는 인덱스를 알려줌(=을 기준으로 인덱스, 값 구분)
-    key = line[:start]            #스타트까지의 값이 키가 되고
-    value = line[start+1:]         #스타트+1부터 끝까지를 값으로
-    env_list[key] = value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DjangoSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -33,8 +24,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
