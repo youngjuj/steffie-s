1fea17c4dccf9a7ef4187612004c6e7444d59a67
youngjuj <alti100@likelion.org>
2021-10-11 21:28:44 +0900

    2021/10/11 probieren

---
+++ static/js/sketch.js
@@ -0,0 +1,21 @@
+const inputImg = document.getElementById('inputImg'); // The image we want to transfer
+const statusMsg = document.getElementById('statusMsg'); // The status message
+const styleA = document.getElementById('styleA'); // The div contrianer that holds new style image A
+const styleB = document.getElementById('styleB'); // The div contrianer that holds new style image B
+
+ml5.styleTransfer('models/wave')
+  .then(style1 => style1.transfer(inputImg))
+  .then(result => {
+    const newImage1 = new Image(250, 250);
+    newImage1.src = result.src;
+    styleA.appendChild(newImage1);
+  });
+
+ml5.styleTransfer('models/udnie')
+  .then(style2 => style2.transfer(inputImg))
+  .then(result => {
+    const newImage2 = new Image(250, 250);
+    newImage2.src = result.src;
+    styleB.appendChild(newImage2);
+    statusMsg.innerHTML = 'Done!';
+  });
